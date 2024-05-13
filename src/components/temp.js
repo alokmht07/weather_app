acc50f860f47417861a2271f5ff1644ea53777e3
Alok Mahato <alokmahato861@gmail.com>
2024-05-13 08:48:37 +0530

    first_commit

---
+++ src/components/temp.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import Weathercard from "./weathercard";
+import "./style.css";
+
+const Temp = () => {
+  const [searchValue, setSearchValue] = useState("Jamshedpur");
+  const [tempInfo, setTempInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=2db59d08a5a8c8fbde4653698179cabb`;
+
+      let res = await fetch(url);
+      let data = await res.json();
+
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleEnterKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      getWeatherInfo();
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            onKeyPress={handleEnterKeyPress}
+          />
+
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}>
+            Search
+          </button>
+        </div>
+      </div>
+
+      {/* our temp card  */}
+      <Weathercard {...tempInfo} />
+    </>
+  );
+};
+
+export default Temp;
